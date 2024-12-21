f47835049db566de3a79fa31b01e04af3999ab06
Natm777 <namoal.pro@gmail.com>
2024-12-21 21:30:52 +0100

    Add mobile logo to Footer and enhance responsive styles for mobile layout

--- kasa/src/components/Footer/index.jsx
+++ kasa/src/components/Footer/index.jsx
@@ -9,6 +9,8 @@ function Footer() {
         <div className="footer">
             
             <img className="footer_img" src="/footer.png" alt="footer_banner"/>
+
+            <img className="logo_mobile" src="/logo_mobile.png" alt="footer_banner"/>
             
         </div>
     );
