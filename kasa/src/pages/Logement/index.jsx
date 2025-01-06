ba05e9245a3bfd373ae7105406a98dbc9be413fd
Natm777 <namoal.pro@gmail.com>
2025-01-06 10:05:17 +0100

    Refactor Logement component: remove unused navigate hook, update error handling to redirect to Error page, and enhance comments for clarity

--- kasa/src/pages/Logement/index.jsx
+++ kasa/src/pages/Logement/index.jsx
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import Banner from "../../components/Banner";
 import RentalInfo from "../../components/RentalInfo";
 import OwnerInfo from "../../components/OwnerInfo";
 import DetailsSection from "../../components/DetailsSection";
-import Error from "../../components/Error";
 import Tags from "../../components/Tags";
 import Stars from "../../components/Stars";
 import Starsmobile from "../../components/Starsmobile";
 import Owner from "../../components/Owner";
 
 function Logement() {
-  const navigate = useNavigate(); // Hook pour la navigation
   const [isLoading, setIsLoading] = useState(true); // État pour indiquer le chargement des données
   const [error, setError] = useState(null); // État pour gérer les erreurs
   const { logementId } = useParams(); // Récupération du paramètre dynamique logementId depuis l'URL
@@ -36,8 +34,9 @@ function Logement() {
         }
         setRental(data); // Mise à jour des données du logement
         const tempStars = []; // Initialisation d'un tableau temporaire pour les étoiles de notation
-        for (let i = 0; i < 5; i++) { // Boucle pour remplir le tableau 
-            tempStars.push(
+        for (let i = 0; i < 5; i++) {
+          // Boucle pour remplir le tableau
+          tempStars.push(
             i < data.rating ? "fa-solid fa-star" : "fa-solid fa-star empty" // Si l'index est inférieur à la note (rating), ajouter une étoile pleine, sinon une étoile vide
           );
         }
@@ -48,22 +47,22 @@ function Logement() {
         setError(error.message); // Gestion des erreurs
         setIsLoading(false); // Fin du chargement
       });
-      }, [logementId, navigate]); // Exécuté à chaque changement de logementId ou navigate
+  }, [logementId]); // Exécuté à chaque changement de logementId 
 
-     // Fonction pour obtenir le contenu des équipements
-     function getEquipmentContent() {
-     // Vérifie si rental.equipments existe et contient un ou plusieurs éléments
-      if (rental.equipments && rental.equipments.length > 0) {
+  // Fonction pour obtenir le contenu des équipements
+  function getEquipmentContent() {
+    // Vérifie si rental.equipments existe et contient un ou plusieurs éléments
+    if (rental.equipments && rental.equipments.length > 0) {
       // Si oui, retourne le tableau des équipements
       return rental.equipments;
     }
     return ["Aucun équipement disponible"]; // Sinon, retourne un tableau avec un message indiquant qu'aucun équipement n'est disponible
-    }
+  }
 
-    if (isLoading) return <p>Chargement...</p>; // Si les données sont en cours de chargement, afficher un message de chargement
-    if (error) return <Error message={error} />; // Si une erreur s'est produite, afficher un composant Error avec le message d'erreur
+  if (isLoading) return <p>Chargement...</p>; // Si les données sont en cours de chargement, afficher un message de chargement
+  if (error) return <Navigate to="/error" />; // Si une erreur s'est produite, rediriger vers la page d'erreur 404
 
-    return (
+  return (
     <div className="rental">
       {/* Affichage du carrousel de photos */}
       <Banner images={rental.pictures} className="carousel-banner" />{" "}
