f79d36679e0f35301122466f63db094e899287c9
Natm777 <namoal.pro@gmail.com>
2025-01-13 20:46:16 +0100

    Refactor APropos component: update Banner image source to a hosted URL and improve formatting of DetailsSection rendering

--- kasa/src/pages/Apropos/index.jsx
+++ kasa/src/pages/Apropos/index.jsx
@@ -7,12 +7,18 @@ function APropos() {
   return (
     <div className="apropospage">
       {/* Bannière pour la page A Propos */}
-      <Banner imageSrc="/image_2.png" className="apropos-banner" />
+      <Banner
+        imageSrc="https://s3-alpha-sig.figma.com/img/56fa/e17e/b9995860bb6384a77ca7dc9bf52da3be?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ACoa1Va0JpxBwdhmLB3WkfX2VpKSPnxjrLMe4-IGwe2E~UOAalmBd0xIpHQP7mp0brDqCg5g~ySIrTQ2GzOfAZc9-WyLv1GqKOfBgny3sFjrITaygI9GA5zs8qnAbw6OoxCULB7qkLKyYhP-KXnDlpJBRHvmkLEdshoVdichnDcVKPExc~MlZx1MD4Cir6PP8wgJmD-a8Q1O~Yz9ARILQ2IyVnqQhwjA1yfrcUD6~g0Fvp~JYhifGC2gSGDHAFYb4Ifj2LILhBH9X9QO2fpgWehPzWpYbZjLCIPB2D~YSfpp1O8WNpsBGXgMjGdmwhByIk6UaH8eFQvVw-abcak1Vw"
+      />
 
       {/* Section des détails */}
       <section className="about-details">
         {aboutList.map((item, index) => (
-          <DetailsSection key={index} title={item.title} content={item.content} />
+          <DetailsSection
+            key={index}
+            title={item.title}
+            content={item.content}
+          />
         ))}
       </section>
     </div>
